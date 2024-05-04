6fb080e849d28bfda790af8d86d0dae64e63f2ee
sola <119765665+solara39@users.noreply.github.com>
2024-05-04 16:34:41 +0900

    Add files via upload

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ SRCS = srcs/init.c\
 LIBFT_PATH = include/libft
 LIBFT = include/libft/libft.a
 MLX_PATH = lib/minilibx
-MLX_FLAGS = -L$(MLX_PATH)
+MLX_FLAGS = -L$(MLX_PATH) -I$(MLX_PATH)
 MLX_COM = -Lmlx -lmlx -framework OpenGL -framework AppKit
 MLX = lib/minilibx/libmlx.a
 
@@ -22,11 +22,12 @@ all: $(NAME)
 %.o: %.c
 	$(CC) $(CFLAGS) -Imlx -I$(LIBFT_PATH) -c $< -o $@
 
-$(NAME): $(MAIN)
-	$(CC) $(MAIN) $(SRCS) $(LIBFT) -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
-
 #$(NAME): $(MAIN)
-#	$(CC) $(MAIN) -o $@ $^ $(SRCS) $(LIBFT_PATH) $(MLX_COM) -o $(NAME)
+#	$(CC) $(MAIN) $(SRCS) $(LIBFT_PATH) -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+
+$(NAME): $(MAIN)
+	@make -C $(LIBFT_PATH)
+	$(CC) $(MAIN) $(SRCS) $(LIBFT) $(MLX_FLAGS) $(MLX_COM) -o $(NAME)
 
 
 clean:
--- main.c
+++ main.c
@@ -3,32 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skondo <skondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: skondo <skondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 16:56:46 by skondo            #+#    #+#             */
-/*   Updated: 2024/04/29 19:29:57 by skondo           ###   ########.fr       */
+/*   Updated: 2024/05/04 16:33:15 by skondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
 # include "include/fractol.h"
 
 int draw_fractol(t_data *fractol, char *argv)
 {
-  if (ft_strncmp(argv, "mandelbrot", 10) == 0) // made
-    draw_mandelbrot(fractol);
-  else if (ft_strncmp(argv, "julia", 5) == 0)
-  {
-    init_fractol(fractol);
-    draw_julia(fractol);
-  }
-  else
-  {
-    //ft_putendl_fd("not appropriate argument", 1);
-    exit_fractol(fractol);
-  }
-  mlx_put_image_to_window(fractol->mlx, fractol->window, fractol->image, 0, 0);
-  return (0);
+	if (ft_strncmp(argv, "mandelbrot", 10) == 0)
+    	draw_mandelbrot(fractol);
+	else if (ft_strncmp(argv, "julia", 5) == 0)
+	{
+		if (!fractol->c_r && !fractol->c_i)
+		{
+			fractol->c_r = -0.745429;
+			fractol->c_i = 0.05;
+		}
+    	draw_julia(fractol);
+	}
+	else
+	{
+		ft_putendl_fd("not appropriate argument", 1);
+		exit_fractol(fractol);
+	}
+	mlx_put_image_to_window(fractol->mlx, fractol->window, fractol->image, 0, 0);
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -37,7 +40,7 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		//ft_putendl_fd("inappropriate arguments", 1);
+		ft_putendl_fd("inappropriate arguments", 1);
 		return (0);
 	}
 	fractol = malloc(sizeof(t_data));
@@ -45,7 +48,7 @@ int	main(int argc, char **argv)
 		return (0);
 	init_fractol(fractol);
 	init_mlx(fractol);
-	//mlx_key_hook(fractol->window, handle_key, fractol);
+	mlx_key_hook(fractol->window, handle_key, fractol);
 	mlx_mouse_hook(fractol->window, handle_mouse, fractol);
 	mlx_hook(fractol->window, 17, 0L, exit_fractol, fractol);
 	draw_fractol(fractol, argv[1]);
